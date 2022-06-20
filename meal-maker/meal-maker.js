bd4ce5c0ebef3afe83c3caf565d7357d457d4f6a
Goose-code23 <76187202+Goose-code23@users.noreply.github.com>
2022-06-20 14:26:26 -0700

    Add files via upload

---
+++ meal-maker/meal-maker.js
@@ -0,0 +1,32 @@
+let menu = {
+    _menu: "",
+    _price: 0,
+  
+    set meal(mealToCheck) {
+      if (typeof mealToCheck === "string") {
+        return (this._meal = mealToCheck);
+      }
+    },
+  
+    set price(priceToCheck) {
+      if (typeof priceToCheck === "number") {
+        return (this._price = priceToCheck);
+      }
+    },
+  
+    get todaysSpecial(){
+      if(this._meal && this._price){
+        return `Today’s special is ${this._meal} for ${this._price}$`;
+      }else{
+        return 'Meal or price was not set correctly!';
+      }
+    }
+  };
+  
+  menu.meal = "burgers";
+  menu.price = 100;
+  
+  console.log(menu);
+  
+  console.log(menu.todaysSpecial);
+  
